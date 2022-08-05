b0332d39180579729b32e5bbfc7792866200f84d
ArtyomDey <a.dey@mail.ru>
2022-08-05 18:18:46 +0300

    Example 006 Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 101;
+int b = 52;
+int c = 856;
+int d = 48;
+int e = 450;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+ 
